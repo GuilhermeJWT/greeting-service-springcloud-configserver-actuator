294f71c94ac1544f9cc8e4d11affde9a9af53c05
GuilhermeJWT <guiromanno@gmail.com>
2021-06-06 08:07:55 -0300

    Definindo algumas Configurações

--- src/main/java/br/com/systemsgs/controller/GreetingController.java
+++ src/main/java/br/com/systemsgs/controller/GreetingController.java
@@ -2,16 +2,24 @@ package br.com.systemsgs.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.systemsgs.configuration.GreetingConfiguration;
 import br.com.systemsgs.model.ModelGreeting;
 
 @RestController
+@RequestMapping(value = "/greeting")
 public class GreetingController {
 	
-	private static final String template = "Guilherme, %s";
+	private static final String template = " %s, %s";
 	private final AtomicLong contador = new AtomicLong();
+	
+	@Autowired
+	private GreetingConfiguration configuration;
+	
 	
 	public ModelGreeting greeting (@RequestParam(value = "name", defaultValue = "Nome Gui") String name) {
 		return new ModelGreeting(contador.incrementAndGet(), String.format(template, name));
